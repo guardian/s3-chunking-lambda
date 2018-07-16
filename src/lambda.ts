4cbdce2957d3e300c76c358438c3635cc7d9504b
tomrf1 <tom.forbes@theguardian.com>
2018-07-16 10:07:34 +0100

    stream and split

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,30 +1,75 @@
+import {Readable} from "stream";
+
+let fs = require('fs');
+let readline = require('readline');
 let AWS = require('aws-sdk');
 let s3 = new AWS.S3();
+
+let MaxKb = parseInt(process.env.MaxObjectSize)*1000;
 
 export function handler(event) {
     console.log("bucket: "+ process.env.Bucket)
     event.Records.map(record => {
         let bucket = record.s3.bucket.name;
         let key = record.s3.object.key;
-        let size = record.s3.object.size;
+        let sizeKb = record.s3.object.size;
 
-        console.log(`${bucket} / ${key} / ${size}`);
-
-        copy(bucket, key)
+        console.log(`${bucket} / ${key} / ${sizeKb}`);
 
         //Took 197870ms to do 9mb
-        //TODO - what is size unit?
-        // if (size > 0) {
-        // } else {
-        // }
+        //23mb file has 21262 lines. 8000 lines per file should be fine
+        if (sizeKb > MaxKb) {
+            console.log("too big")
+            splitFile(bucket, key)
+        } else {
+            console.log("not too big")
+            copy(bucket, key)
+        }
     })
 }
 
-// function get(bucket: string, ) {
-//     s3.getObject({
-//         Bucket:
-//     })
-// }
+function splitFile(sourceBucket: string, key: string) {
+    let inputStream: Readable = s3.getObject({
+        Bucket: sourceBucket,
+        Key: key
+    }).createReadStream();
+
+    //We'll reassign a new stream to `outputStream` each time we hit MaxKb
+    let fileNumber = 0;
+    function createNewWriteStream() {
+        let stream = fs.createWriteStream(`/tmp/${key}`);
+        s3.putObject({
+            Bucket: process.env.Bucket,
+            Key: `${sourceBucket}/${key}_chunk-${fileNumber}`,
+            Body: stream
+        }, function(err, data) {
+            if (err) console.log(err)
+            else console.log(data)
+        });
+
+        return stream;
+    }
+
+    let outputStream = createNewWriteStream();
+
+    //outputStream.on('open', function() {
+        //console.log('outputStream open')
+        let reader = readline.createInterface({
+            input: inputStream
+        });
+
+        reader.on('line', function(line: string) {
+            outputStream.write(line + '\n');
+
+            if (outputStream.bytesWritten > MaxKb*1000) {
+                console.log(`closing output stream at ${outputStream.bytesWritten}`);
+                outputStream.close();
+
+                outputStream = createNewWriteStream();
+            }
+        });
+    //});
+}
 
 function copy(sourceBucket: string, key: string) {
     s3.copyObject({
